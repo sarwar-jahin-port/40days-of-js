ce2ba1e13085c443648e1915fcf1a1fa5586910b
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-07-31 22:35:10 +0600

    day 02 task done

---
+++ day2/index.js
@@ -0,0 +1,19 @@
+console.log("40 days of Javascript: Day 02");
+
+// Task 1:
+let name = "sarwar";
+let age = 24;
+let isStudent = true;
+let favLang = "JavaScript";
+// Task 2:
+console.log(name, age, isStudent, favLang);
+// Task 3:
+let name = "jahin";
+const age = 40;
+// Task 4:
+const person = {
+    name: "sarwar",
+    age: 24,
+    isStudent: true,
+    favLang: "JavaScript",
+}
